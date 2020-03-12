024b6bbab99e7c6769a91073c96087336c33be0c
Gregor <gregor.soln@gmail.com>
2020-03-12 14:58:37 +0000

    initial commit

---
+++ run.ps1
@@ -0,0 +1,16 @@
+﻿Get-NAVAppInfo -ServerInstance <server instance name> -Tenant <tenant ID> | % { Uninstall-NAVApp -ServerInstance <server instance name> -Tenant <tenant ID> -Name $_.Name -Version $_.Version -Force}
+Get-NAVAppInfo -ServerInstance <server instance name> -Tenant <tenant ID> | % { Uninstall-NAVApp -ServerInstance <server instance name> -Tenant <tenant ID> -Name $_.Name -Version $_.Version -Force}
+
+Get-NAVAppInfo -ServerInstance <server instance name>
+
+Unpublish-NAVApp -ServerInstance <server instance> -Name System -version <version>
+
+
+Stop-NAVServerInstance -ServerInstance <server instance>
+# Install Business Central
+
+Invoke-NAVApplicationDatabaseConversion -DatabaseServer <database server name>\<database server instance> -DatabaseName "<database name>"
+Set-NAVServerConfiguration -ServerInstance <server instance> -KeyName DatabaseName -KeyValue "<database name>"
+Restart-NAVServerInstance -ServerInstance <server instance>
+
+Publish-NAVApp -ServerInstance <server instance> -Path "<path to the System.app file>" -PackageType SymbolsOnly
